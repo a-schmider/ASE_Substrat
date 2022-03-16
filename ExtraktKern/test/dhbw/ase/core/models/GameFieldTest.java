a907b9013ac429bd01e5ca58011ca1e597be2c8c
ANSR <andreas.schmider98@gmail.com>
2022-03-16 11:31:42 +0100

    Junit und Mockito hinzugefügt für Tests

---
+++ ExtraktKern/test/dhbw/ase/core/models/GameFieldTest.java
@@ -0,0 +1,15 @@
+package dhbw.ase.core.models;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+class GameFieldTest {
+
+    @Test
+    public void isFreeTest() {
+        GameField s = Mockito.mock(GameField.class);
+        GameField p = new GameField();
+
+
+    }
+}
